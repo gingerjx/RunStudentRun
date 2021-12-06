ec9a4e1fdfa674e5d86d137f99d4ec6c66909c49
Piotr Kalota <piotrkalota99@gmail.com>
2021-12-06 16:43:22 +0100

    Add semester counting

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,13 +5,23 @@ using UnityEngine.UI;
 
 public static class GameController
 {
+    const int SEMESTER_PASS = 4; // For debugging purposes, will be changed to 30
+
+    static int semester = 1;
     static int ects = 0;
     static int hp = 3;
 
     public static void addEcts(int pts)
     {
         ects += pts;
+        if (ects >= SEMESTER_PASS)
+        {
+            ects -= SEMESTER_PASS;
+            semester += 1;
+        }
+        
         GameObject.Find("Ects").GetComponent<Text>().text = "Ects: " + ects;
+        GameObject.Find("Semester").GetComponent<Text>().text = "Sem: " + semester;
     }
     public static void damage()
     {
