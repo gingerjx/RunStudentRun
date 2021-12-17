2b7f2fef8deaa501865c57695ebd32eeb0617e2c
Piotr Kalota <piotrkalota99@gmail.com>
2021-12-17 19:02:56 +0100

    Add student walk animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,7 @@ public class PlayerMovement : MonoBehaviour
     Vector2 touchPosition;
     float moving;
     private AudioSource audioSource;
+    private Animator animator;
 
     // Start is called before the first frame update
     private void Awake()
@@ -33,22 +34,30 @@ public class PlayerMovement : MonoBehaviour
         p1Zone = new Rect(0, 0, Screen.width * 0.5f, Screen.height);
         p2Zone = new Rect(Screen.width * 0.5f, 0, Screen.width * 0.5f, Screen.height);
         audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         touchPosition = playerInputActions.Player.Move.ReadValue<Vector2>();
         moving = playerInputActions.Player.Touch.ReadValue<float>();
+
         if (moving > 0)
         {
+            animator.speed = 1;
+            
             if (p1Zone.Contains(touchPosition) && transform.localPosition.x > -Screen.width * 0.5) // w lewo
             {
+                animator.SetFloat("Move X", -1);
                 transform.position = transform.position + new Vector3(-movementSpeed * Time.deltaTime, 0, 0);
             }
             else if (p2Zone.Contains(touchPosition) && transform.localPosition.x < Screen.width * 0.5) // w prawo
             {
+                animator.SetFloat("Move X", 1);
                 transform.position = transform.position + new Vector3(movementSpeed * Time.deltaTime, 0, 0);
             }
+        } else {
+            animator.speed = 0;
         }
     }
 
