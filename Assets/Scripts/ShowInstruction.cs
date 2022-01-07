6c66679c3adf952d415441851a02b3c21a843c61
KacperKluska <kacperkluska130@gmail.com>
2022-01-07 17:46:11 +0100

    Added game info screen to Pause screen

--- Assets/Scripts/ShowInstruction.cs
+++ Assets/Scripts/ShowInstruction.cs
@@ -7,8 +7,13 @@ public class ShowInstruction : MonoBehaviour
 {
     public void handleInstruction()
     {
-        Canvas mainCanvas = GameObject.Find("Canvas").GetComponent<Canvas>();
         Canvas instructionCanvas = GameObject.Find("InstructionCanvas").GetComponent<Canvas>();
+        GameObject mainCanvasObject = GameObject.Find("Canvas");
+        if (mainCanvasObject == null) // jeżeli jesteśmy w menu pauzy 
+        {
+            mainCanvasObject = GameObject.Find("PauseCanvas");
+        }
+        Canvas mainCanvas = mainCanvasObject.GetComponent<Canvas>();
         if (mainCanvas.enabled)
         {
             mainCanvas.enabled = false;
